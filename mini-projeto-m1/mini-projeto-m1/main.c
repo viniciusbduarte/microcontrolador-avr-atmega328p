7faceba459c2a86a5ca651525b0e40b70e755dcf
Vinicius Batista Duarte <vg39348@gmail.com>
2026-02-02 16:45:38 -0300

    feat: create

---
+++ mini-projeto-m1/mini-projeto-m1/main.c
@@ -0,0 +1,18 @@
+/*
+ * mini-projeto-m1.c
+ *
+ * Created: 02/02/2026 15:47:05
+ * Author : Vinicius Duarte
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
